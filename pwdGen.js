d28686ac5893a8645f0dbf1ab0ebb2c25b4a2793
AlessandroSestili <sestilialessandro@gmail.com>
2021-04-07 15:34:03 +0200

    implementa strong, finisce progetto

--- pwdGen.js
+++ pwdGen.js
@@ -2,4 +2,4 @@ var nome = prompt("Qual'è il tuo nome")
 var cognome = prompt("Qual'è il tuo cognome?")
 var colore = prompt("Qual'è il tuo colore preferito?")
 
-document.getElementById ("risultato").innerHTML = `${nome} ${cognome} ${colore}`
+document.getElementById ("risultato").innerHTML = `${nome} ${cognome} <strong>${colore}</strong>`
